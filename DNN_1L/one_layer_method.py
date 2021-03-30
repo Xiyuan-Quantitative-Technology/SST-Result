017767886dad7a44f30faeb507e485c6cd3823ce
Xiyuan-Quantitative-Technology <81297269+Xiyuan-Quantitative-Technology@users.noreply.github.com>
2021-03-30 12:49:17 +0800

    Add files via upload

---
+++ DNN_1L/one_layer_method.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+class One_layer_meth:
+    def __init__(self):
+        self.n_feature = 1024
+
+        self.n_hidden_1 = 1024
+
+        self.n_output=1
+        
+        self.lr = 0.001
+	
+        self.epoch = 100
+
+
+    def set_Net(self,n_feature,n_hidden_1,n_output):
+    	self.n_feature=int(n_feature)
+    	self.n_hidden_1= int(n_hidden_1)
+    	self.n_output=int(n_output)
+        
+    def set_epoch(self,epoch):
+        self.epoch = int(epoch)
+        
+    def set_lr(self,lr):
+        self.lr = lr   
+    
+    def one_layer_net(self,X_data,y_data):
+        import torch
+        import torch.nn as nn
+        from one_layer_module import Net
+        
+        model = Net(self.n_feature,self.n_hidden_1,self.n_output)
+    
+        optimizers = torch.optim.Adam(model.parameters(),lr=self.lr,betas=(0.9,0.99))
+
+        loss_func = nn.MSELoss()
+
+        #losses = []
+        for epoch_ in range(self.epoch):  
+            
+            output = model(X_data) # get output for every net
+            loss = loss_func(output,y_data) # compute loss for every net
+            optimizers.zero_grad() # claer gradients for net train
+            loss.backward() # backpropagation, compute gradients
+            optimizers.step() # apply gradients
+            
+            #losses.append(loss.item())
+            
+        return model
+    
---
+++ DNN_1L/one_layer_module.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self, n_features,n_hidden_1,n_output):
+        super(Net, self).__init__()
+        self.model = nn.Sequential(nn.Linear(n_features,n_hidden_1), 
+                                   nn.ReLU(),
+                                   nn.Linear(n_hidden_1, n_output))
+        
+    def forward(self, input1):
+        output = self.model(input1)
+        return output
+
